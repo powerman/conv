21c44060b701d2db7075073fe043c86e8a5e059b
Alex Efros <powerman@powerman.name>
2021-03-23 20:18:38 +0200

    feat: add Time<->UnixMs

---
+++ native.go
@@ -0,0 +1,22 @@
+package conv
+
+import "time"
+
+// TimeFromUnixMs converts unix milliseconds to time.
+// Returns zero time if v is 0 (instead of 1970-01-01).
+func TimeFromUnixMs(v int64) time.Time {
+	const ms = 1000
+	if v == 0 {
+		return time.Time{}
+	}
+	return time.Unix(v/ms, (v%ms)*int64(time.Millisecond))
+}
+
+// UnixMsFromTime converts time to unix milliseconds.
+// Returns 0 both for zero time (instead of -6795364578871) and zero unixtime (1970-01-01).
+func UnixMsFromTime(v time.Time) int64 {
+	if v.IsZero() {
+		return 0
+	}
+	return v.UnixNano() / int64(time.Millisecond)
+}
---
+++ native_test.go
@@ -0,0 +1,27 @@
+package conv_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/powerman/check"
+
+	"github.com/powerman/conv"
+)
+
+func TestTimeFromUnixMs(tt *testing.T) {
+	t := check.T(tt)
+	t.Parallel()
+	t.Equal(conv.TimeFromUnixMs(1616522747123).UTC(), time.Date(2021, 3, 23, 18, 5, 47, 123000000, time.UTC))
+	t.Equal(conv.TimeFromUnixMs(1).UTC(), time.Date(1970, 1, 1, 0, 0, 0, 1000000, time.UTC))
+	t.Equal(conv.TimeFromUnixMs(0).UTC(), time.Time{})
+}
+
+func TestUnixMsFromTime(tt *testing.T) {
+	t := check.T(tt)
+	t.Parallel()
+	t.Equal(conv.UnixMsFromTime(time.Date(2021, 3, 23, 18, 5, 47, 123987654, time.UTC)), int64(1616522747123))
+	t.Equal(conv.UnixMsFromTime(time.Date(1970, 1, 1, 0, 0, 0, 1987654, time.UTC)), int64(1))
+	t.Equal(conv.UnixMsFromTime(time.Date(1970, 1, 1, 0, 0, 0, 987654, time.UTC)), int64(0))
+	t.Equal(conv.UnixMsFromTime(time.Time{}), int64(0))
+}
